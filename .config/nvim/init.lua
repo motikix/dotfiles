d4d3a781161c7632a8ec19ccce7e7e19d8242018
syuni <motikix@gmail.com>
2021-07-19 23:12:35 +0900

    fix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -51,8 +51,8 @@ vim.o.laststatus = 2
 
 -- Indent
 vim.o.tabstop = 4
-vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
+vim.o.smarttab = true
 vim.o.expandtab = true
 vim.o.autoindent = true
 vim.o.smartindent = true
