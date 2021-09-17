0c8037ec72b3df6bf7c5110c15f4307d1c444509
syuni <motikix@gmail.com>
2021-09-17 14:47:21 +0900

    fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -26,6 +26,9 @@ set -x BRIGHT_WHITE_COLOR '#ffffff'
 # lang
 set -x LANG en_US.UTF-8
 
+# gpg
+set -x GPG_TTY (tty)
+
 # xdg base directory specification
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_CACHE_HOME $HOME/.cache
