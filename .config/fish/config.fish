314a4c7fdf5158e02d30b45d77eed90d647bbecb
INOUE-Syun <xinoue.syun@contract.isid.co.jp>
2021-11-02 14:13:58 +0900

    fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,121 +18,122 @@ set -x BRIGHT_MAGENTA_COLOR '#dcbdfb'
 set -x BRIGHT_CYAN_COLOR '#56d4dd'
 set -x BRIGHT_WHITE_COLOR '#768390'
 
-# proxy
-set -x no_proxy "localhost,127.0.0.1,*.dentsu.co.jp,*.isid.co.jp"
-set -x http_proxy "http://133.158.251.200:8080"
-set -x https_proxy "http://133.158.251.200:8080"
-
 # environments
 
-# lang
+## lang
 set -x LANG en_US.UTF-8
 
-# gpg
+## gpg
 set -x GPG_TTY (tty)
 
-# editor
+## editor
 set -x EDITOR /usr/bin/vim
 
-# xdg base directory specification
+## xdg base directory specification
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_CACHE_HOME $HOME/.cache
 set -x XDG_DATA_HOME $HOME/.local/share
 set -x XDG_STATE_HOME $HOME/.local/state
 
-# bin
+## bin
 set -x PATH $HOME/.local/bin $PATH
 
-# ssh-agent
+## ssh-agent
 if type -q ssh-agent
   eval (ssh-agent -c) > /dev/null
 end
 
-# asdf
+## asdf
 if test -d $HOME/.asdf
   source $HOME/.asdf/asdf.fish
 end
 
-# java
+## java
 if test "$XDG_SESSION_DESKTOP" = "sway"
   set -x _JAVA_AWT_WM_NONREPARENTING 1
 end
 
-# go
+## go
 set -x GOPATH $HOME/go
 set -x PATH $GOPATH/bin $PATH
 
-# rust
+## rust
 set -x PATH $HOME/.cargo/bin $PATH
 
-# haskell
+## haskell
 # set -x PATH $HOME/.cabal/bin $PATH
 # set -x PATH $HOME/.ghcup/bin $PATH
 
-# node
+## node
 set -x NPM_CONFIG_PREFIX $HOME/.local
 
-# deno
+## deno
 set -x DENO_INSTALL_ROOT $HOME/.local
 
-# python
+## python
 set -x PIPENV_VENV_IN_PROJECT true
 
-# flutter
+## flutter
 set -x PATH $HOME/flutter/bin $PATH
 
-# fzf
+## fzf
 set -x FZF_LEGACY_KEYBINDINGS 1
 set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --glob "!.git"'
 set -x FZF_DEFAULT_OPTS '--height 40% --layout=reverse --border --preview "head -n 30 {}"'
 
 # aliases
 
-# neovim
+## neovim
 if type -q nvim
   alias vim="nvim"
 end
 
-# colordiff
+## colordiff
 if type -q colordiff
   alias diff="colordiff -up"
 else
   alias diff="diff -up"
 end
 
-# exa
+## exa
 if type -q exa
   alias ls="exa"
   alias ll="exa -lh -s date -s new --git"
   alias tree="exa -T"
 end
 
-# diff-so-fancy
+## diff-so-fancy
 if type -q diff-so-fancy
   alias dsf="diff-so-fancy"
 end
 
-# launch
-
 # company settings (on WSL2)
-# path
+
+## proxy
+set -x no_proxy "localhost,127.0.0.1,*.dentsu.co.jp,*.isid.co.jp"
+set -x http_proxy "http://133.158.251.200:8080"
+set -x https_proxy "http://133.158.251.200:8080"
+## path
 set -x PATH /mnt/d/bin $PATH
-# aws-vault
+set -x PATH /mnt/d/win32yank $PATH
+## aws-vault
 set -x AWS_VAULT_BACKEND pass
 set -x AWS_VAULT_PASS_PREFIX aws-vault
-# nodejs
+## nodejs
 set -x NODE_TLS_REJECT_UNAUTHORIZED 0
-
-if not set -q INSIDE_GENIE
+## launch genie
+if type -q genie; and not set -q INSIDE_GENIE
   exec genie -s
 end
 
-# starship
+# launch
+
+## starship
 if type -q starship
   starship init fish | source
 end
 
-# tmux
+## tmux
 if status is-interactive
 and not set -q TMUX
     exec tmux
