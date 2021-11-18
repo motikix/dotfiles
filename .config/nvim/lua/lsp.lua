7041ed66cd814fed90ab6e05af10b7d77bd9ddad
INOUE-Syun <xinoue.syun@contract.isid.co.jp>
2021-11-18 11:33:13 +0900

    fix lsp_sugnature floating window config

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -34,7 +34,7 @@ local on_attach = function(client, bufnr)
   require('lsp_signature').on_attach({
       bind = true,
       doc_lines = 10,
-      floating_window = false,
+      floating_window = true,
       fix_pos = false,
       hint_enable = true,
       hint_prefix = require('config').sign.hint..' ',
