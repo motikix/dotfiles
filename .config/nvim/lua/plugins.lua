7c62f5190cfcbecc90f81cd458ec0a34cdfee939
syuni <motikix@gmail.com>
2021-12-17 20:35:04 +0900

    fix

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -496,9 +496,31 @@ require('packer').startup(function()
   use {
     'nvim-neorg/neorg',
     requires = { 'nvim-lua/plenary.nvim' },
-    setup = vim.cmd('autocmd BufRead,BufNewFile *.norg setlocal filetype=norg'),
     after = { 'nvim-treesitter' },
-    ft = 'norg',
+    setup = function()
+      local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
+      parser_configs.norg = {
+        install_info = {
+          url = 'https://github.com/nvim-neorg/tree-sitter-norg',
+          files = { 'src/parser.c', 'src/scanner.cc' },
+          branch = 'main',
+        },
+      }
+      parser_configs.norg_meta = {
+        install_info = {
+          url = 'https://github.com/nvim-neorg/tree-sitter-norg-meta',
+          files = { 'src/parser.c' },
+          branch = 'main',
+        },
+      }
+      parser_configs.norg_table = {
+        install_info = {
+          url = 'https://github.com/nvim-neorg/tree-sitter-norg-table',
+          files = { 'src/parser.c' },
+          branch = 'main',
+        },
+      }
+    end,
     config = function()
       require('neorg').setup{
         load = {
