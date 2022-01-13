cc89b0931a4274bc9515369f47bfa2bb61ac86b9
syuni <motikix@gmail.com>
2022-01-13 15:49:51 +0900

    fix(.config/nvim): fix nvim-cmp keybinding

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -421,7 +421,7 @@ require('packer').startup(function()
           end,
         },
         mapping = {
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.close(),
