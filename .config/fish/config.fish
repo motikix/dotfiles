b5c68048dcb7824e07ebc23781542c7c0e32f0d5
syuni <motikix@gmail.com>
2022-04-22 10:45:20 +0900

    fix(nvim):

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,26 +1,6 @@
 # suppress greeting message
 set fish_greeting
 
-# theme (GitHub Dimmed)
-# set -x BACKGROUND_COLOR '#22272e'
-# set -x FOREGROUND_COLOR '#768390'
-# set -x BLACK_COLOR '#22272e'
-# set -x RED_COLOR '#ff938a'
-# set -x GREEN_COLOR '#6bc46d'
-# set -x YELLOW_COLOR '#c69026'
-# set -x BLUE_COLOR '#6cb6ff'
-# set -x MAGENTA_COLOR '#b083f0'
-# set -x CYAN_COLOR '#56d4dd'
-# set -x WHITE_COLOR '#768390'
-# set -x BRIGHT_BLACK_COLOR '#636e7b'
-# set -x BRIGHT_RED_COLOR '#ff938a'
-# set -x BRIGHT_GREEN_COLOR '#6bc46d'
-# set -x BRIGHT_YELLOW_COLOR '#daaa3f'
-# set -x BRIGHT_BLUE_COLOR '#6cb6ff'
-# set -x BRIGHT_MAGENTA_COLOR '#dcbdfb'
-# set -x BRIGHT_CYAN_COLOR '#56d4dd'
-# set -x BRIGHT_WHITE_COLOR '#768390'
-
 # theme (Material Ocean)
 set -x BACKGROUND_COLOR '#0f111a'
 set -x FOREGROUND_COLOR '#a6accd'
@@ -49,10 +29,7 @@ set -x LANG en_US.UTF-8
 ## gpg
 set -x GPG_TTY (tty)
 
-## editor
-set -x EDITOR /usr/bin/vim
-
-## xdg base directory specification
+# xdg base directory specification
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_CACHE_HOME $HOME/.cache
 set -x XDG_DATA_HOME $HOME/.local/share
@@ -128,21 +105,6 @@ end
 ## diff-so-fancy
 if type -q diff-so-fancy
   alias dsf="diff-so-fancy"
-end
-
-# company settings (on WSL2)
-
-## path
-set -x PATH /mnt/d/bin $PATH
-set -x PATH /mnt/d/win32yank $PATH
-## aws-vault
-set -x AWS_VAULT_BACKEND pass
-set -x AWS_VAULT_PASS_PREFIX aws-vault
-## nodejs
-set -x NODE_TLS_REJECT_UNAUTHORIZED 0
-## launch genie
-if type -q genie; and not set -q INSIDE_GENIE
-  exec genie -s
 end
 
 # launch
