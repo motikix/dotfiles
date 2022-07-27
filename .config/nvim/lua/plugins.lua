f3a052b510a5ccd3e1a27afb5fbbe08972c2e71b
syuni <motikix@gmail.com>
2022-07-27 11:03:51 +0900

    fix

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -221,8 +221,8 @@ require('packer').startup({
     use {
       'vimpostor/vim-tpipeline',
       config = function()
-        vim.g.tpipeline_cursormoved = 1
         vim.g.tpipeline_autoembed = 1
+        vim.g.tpipeline_cursormoved = 1
         vim.g.tpipeline_restore = 1
         vim.g.tpipeline_clearstl = 1
       end,
