45862aa1a97cfd66d967e422d810ea2cff862a70
syuni <motikix@gmail.com>
2022-07-27 16:22:37 +0900

    fix

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -234,15 +234,6 @@ require('packer').startup({
         }
       end,
     }
-    use {
-      'vimpostor/vim-tpipeline',
-      config = function()
-        vim.g.tpipeline_autoembed = 1
-        vim.g.tpipeline_cursormoved = 1
-        vim.g.tpipeline_restore = 1
-        vim.g.tpipeline_clearstl = 1
-      end,
-    }
 
     -- finder
     use {
