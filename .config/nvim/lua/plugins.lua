26158d0430d5af4e5efd767780803521d2210dbf
syuni <motikix@gmail.com>
2022-08-25 19:31:24 +0900

    fix(nvim): wilder

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -817,19 +817,14 @@ require('packer').startup({
         })
         wilder.set_option(
           'renderer',
-          wilder.popupmenu_renderer(wilder.popupmenu_palette_theme({
+          wilder.popupmenu_renderer({
             highlighter = wilder.basic_highlighter(),
             highlights = {
               accent = wilder.make_hl('WilderAccent', 'Pmenu', { { a = 1 }, { a = 1 }, { foreground = '#f4468f' } }),
             },
             left = { ' ', wilder.popupmenu_devicons() },
             right = { ' ', wilder.popupmenu_scrollbar() },
-            border = 'rounded',
-            max_height = '75%',
-            min_height = 0,
-            prompt_position = 'top',
-            reverse = 0,
-          }))
+          })
         )
       end,
     })
