4bbba665a32eb1bd7d61d08ac143dc6b56fd03d6
syuni <motikix@gmail.com>
2023-06-21 20:12:52 +0900

    fix

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,14 +1,14 @@
-local wezterm = require("wezterm")
+local wezterm = require('wezterm')
 
 return {
-	font = wezterm.font_with_fallback({'Monaco', 'Noto Sans JP', 'Noto Emoji'}),
-	font_size = 14,
-	color_scheme = "Catppuccin Mocha",
-	enable_tab_bar = false,
-	window_padding = {
-		top = 0,
-		right = 0,
-		bottom = 0,
-		left = 0,
-	},
+  font = wezterm.font_with_fallback({ 'Monaco', 'Noto Sans JP', 'Noto Emoji' }),
+  font_size = 14,
+  color_scheme = 'Catppuccin Mocha',
+  enable_tab_bar = false,
+  window_padding = {
+    top = 0,
+    right = 0,
+    bottom = 0,
+    left = 0,
+  },
 }
