0631c5d52b3e3daa46ebc290b5c219e177f7f736
syuni <motikix@gmail.com>
2023-07-04 23:29:53 +0900

    fix

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,9 @@
 local wezterm = require('wezterm')
 
 return {
-  font = wezterm.font_with_fallback({ 'IntelOne Mono', 'Noto Sans JP', 'Noto Emoji' }),
+  font = wezterm.font_with_fallback({ 'Monaco Nerd Font', 'Noto Sans JP', 'Noto Emoji' }),
   font_size = 14,
+  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
   color_scheme = 'Catppuccin Mocha',
   enable_tab_bar = false,
   window_padding = {
