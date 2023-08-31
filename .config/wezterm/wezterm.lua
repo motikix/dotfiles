c6fee1a0ec01100b3aaa54eee5030fbedbc2173e
syuni <motikix@gmail.com>
2023-08-31 21:35:45 +0900

    change jp font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require('wezterm')
 
 return {
-  font = wezterm.font_with_fallback({ 'IntoneMono Nerd Font', 'Noto Sans JP', 'Noto Emoji' }),
+  font = wezterm.font_with_fallback({ 'IntoneMono Nerd Font', 'Kiwi Maru', 'Noto Emoji' }),
   font_size = 16,
   harfbuzz_features = {},
   color_scheme = 'Catppuccin Mocha',
