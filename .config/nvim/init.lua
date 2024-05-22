0e18126834b94a0f486b08edcaa42a6c3281acaa
motikix <motikix@gmail.com>
2024-05-22 10:30:01 +0900

    fix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -72,14 +72,14 @@ vim.o.concealcursor = nil
 vim.o.clipboard = 'unnamed,unnamedplus'
 if vim.fn.has('wsl') == 1 then
   vim.g.clipboard = {
-    name = 'win32yank',
+    name = 'clip',
     copy = {
       ['+'] = 'win32yank.exe -i --crlf',
-      ['-'] = 'win32yank.exe -i --crlf',
+      ['*'] = 'win32yank.exe -i --crlf',
     },
     paste = {
-      ['+'] = 'win32yank.exe -o --crlf',
-      ['-'] = 'win32yank.exe -o --crlf',
+      ['+'] = 'win32yank.exe -o --lf',
+      ['*'] = 'win32yank.exe -o --lf',
     },
     cache_enabled = 0,
   }
